c19c3ca03cf78ac871358ce0e3d6a7c1b747c026
kicbar <bartlomiejkicior@gmail.com>
2020-12-02 23:37:40 +0100

    add fire-and-forget job

--- Controllers/HangfireController.cs
+++ Controllers/HangfireController.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hangfire;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace hangfire_webapi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class HangfireController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Welcome()
         {
-            return Ok("Hello from hangfire web api");
+            var jobId = BackgroundJob.Enqueue(() => SendWelcomeEmail("Welcome to our app"));
+
+            return Ok($"Job ID: {jobId}. Welcome email sent to the user!");
         }
-    
+
+        public void SendWelcomeEmail(string text)
+        {
+            Console.WriteLine(text);
+        }
     }
 }
