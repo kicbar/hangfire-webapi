40eb8c23c5110fac07d6b14353ee8f0844a9dbd0
kicbar <bartlomiejkicior@gmail.com>
2020-12-02 23:42:44 +0100

    add delayed jobs

--- Controllers/HangfireController.cs
+++ Controllers/HangfireController.cs
@@ -22,6 +22,16 @@ namespace hangfire_webapi.Controllers
             return Ok($"Job ID: {jobId}. Welcome email sent to the user!");
         }
 
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Discount()
+        {
+            int timeInSeconds = 30;
+            var jobId = BackgroundJob.Schedule(() => SendWelcomeEmail("Welcome to our app"), TimeSpan.FromSeconds(timeInSeconds));
+
+            return Ok($"Job ID: {jobId}. Discount email will be sent in {timeInSeconds} seconds!");
+        }
+
         public void SendWelcomeEmail(string text)
         {
             Console.WriteLine(text);
